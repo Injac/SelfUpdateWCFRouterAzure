7ab50e236fb0eef96a38351ad6bce82949293754
Ilija <ilija.injac@outlook.com>
2013-08-28 18:32:32 +0200

    Renamed some files

--- WCFRouter/SelfUpdatingRouter.svc.cs
+++ WCFRouter/SelfUpdatingRouter.svc.cs
@@ -10,7 +10,7 @@ namespace WCFRouter
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : ServiceRouter
+    public class SelfUpdatingRouter : ServiceRouter
     {
         public string GetData(int value)
         {
